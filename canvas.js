da3706358b5b60bc7233c1d28cad7ae5fb28fb0b
antonioii <asergiocosta.as@gmail.com>
2022-09-29 10:03:09 -0300

    Fix bug in creating the grid based on nOfSquares
    Given a number of squares, the software were creating a grid with
    2 lines and nOfSquares columns. Corrected with two for loops
    in order to create a matrix with nOfSquares x nOfSquares size

--- canvas.js
+++ canvas.js
@@ -1,14 +1,23 @@
-//create canvas
 function createCanvas(nOfSquares) {
-    for (let i = 0; i < nOfSquares; i++){
-        let divisor = 100/(nOfSquares/2);
-        let newDiv = document.createElement('div');
-        newDiv.textContent = '.';
-        newDiv.classList.add('canvaSquare');
-        newDiv.style.setProperty('flex',`1 0 ${divisor}%`);    
-        canvas.appendChild(newDiv);
+    //make squares
+    for(let i = 0; i<nOfSquares;i++){
+        //create line based on the number of squares
+        let newLine = document.createElement('div');
+        newLine.textContent = '';
+        newLine.style.setProperty('display',`flex`); 
+        newLine.style.setProperty('flex',`1 1 100%`); 
+        canvas.appendChild(newLine);        
         
+        //create columns (based on the number of squasres for each line
+        for(let j = 0; j<nOfSquares;j++) {
+        let newColumn = document.createElement('div');
+        newColumn.textContent = 'square';
+        newColumn.classList.add('canvasColumn');
+        newColumn.style.setProperty('flex',`1 1 100%`);
+        newLine.appendChild(newColumn);
+        }
     }
+
 }
 
 //number of initial squares inside the canvas
