6c5ac30b6fc8ee58b88bec35888f0e7fb5c55333
antonioii <asergiocosta.as@gmail.com>
2022-09-30 19:16:14 -0300

    Add comments explaining the paint function made

--- paint.js
+++ paint.js
@@ -1,10 +1,13 @@
+//color used to make the paint
 let inkColor = 'black';
 
+//function to paint
 function paint(){
     //take all the columns been currently been displayed, they represents the 'squares' inside the canvas
     let columns = document.querySelectorAll('.canvasColumn');
     //for each column taken, add an event listener
-    columns.forEach(square => {    
+    columns.forEach(square => { 
+        //a mousemove listener with the event.button of a left click inside it to capture the 'click and drag' drawing functionality   
         square.addEventListener('mousemove', function(event) {
             if(event.buttons == 1) {
              event.preventDefault();           
@@ -13,8 +16,6 @@ function paint(){
            });   
     });
 }
-
-
 
 //call the paint function
 paint();
