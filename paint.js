72a8f1c9035bd5a445016057283852f9e5e29d77
antonioii <asergiocosta.as@gmail.com>
2022-09-30 01:44:33 -0300

    Clean the painting functionality code

--- paint.js
+++ paint.js
@@ -1,45 +1,17 @@
 let inkColor = 'black';
 let pressed = 'false';
 
-function pressing(e){
-    if(pressed){
-        pressed = false;
-    } else {
-        pressed = true;
-        painting(e);
-    }
-}
-
-
-function painting(e){
-    if(pressed == true){
-        e.target.style.setProperty('background-color',`${inkColor}`);
-    }
-}
-
 function paint(){
     //take all the columns been currently been displayed, they represents the 'squares' inside the canvas
     let columns = document.querySelectorAll('.canvasColumn');
     //for each column taken, add an event listener
-    columns.forEach(square => {
-        //cover all the user actions trying to paint using a mouse
-        //square.addEventListener('click', painting);
-        //square.addEventListener('mouseup', pressing);
-        //square.addEventListener('mousedown',painting);
-        //square.addEventListener('mouseover', painting);
-    
+    columns.forEach(square => {    
         square.addEventListener('mousemove', function(event) {
             if(event.buttons == 1) {
-             event.preventDefault();
-           
-             square.style.setProperty('background-color',`${inkColor}`);
-           
+             event.preventDefault();           
+             square.style.setProperty('background-color',`${inkColor}`);           
             }
-           });
-    
-    
-    
-    
+           });   
     });
 }
 
